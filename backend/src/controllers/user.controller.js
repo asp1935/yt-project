1643a156c083bb621eda05453b35602d0a6d15c2
asp1935 <akashp1935@gmail.com>
2024-07-29 15:58:03 +0530

    refershAccessToken Controller added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,51 +1,51 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
-import {APIError} from '../utils/APIError.js';
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { APIError } from '../utils/APIError.js';
 import { User } from '../models/user.model.js';
 import uploadOnCloudinary from '../utils/FileUploadCloudinary.js';
 import { APIResponce } from '../utils/APIResponce.js';
-
+import jwt from 'jsonwebtoken';
 
 //this method for genrating refresh token and access token
 
-const genrateAccessAndRefreshToken=async(userId)=>{
+const genrateAccessAndRefreshToken = async (userId) => {
     try {
         //find user 
         // console.log(userId);
-        const user= await User.findById(userId);
+        const user = await User.findById(userId);
 
         //genrate tokens
-        const accessToken= user.genrateAccessToken();
-        const refreshToken= user.genrateRefreshToken();
+        const accessToken = user.genrateAccessToken();
+        const refreshToken = user.genrateRefreshToken();
 
         //store tokens to db 
-        user.refreshToken=refreshToken;
+        user.refreshToken = refreshToken;
         //save method update db and validateBeforeSave:false is given because we are updating only single value thats why it calls mongoose moduls like require like 
         //so we give validateBeforeSave:false it save/update  data without validation  
-        await user.save({validateBeforeSave:false})        
-        return {refreshToken,accessToken}
+        await user.save({ validateBeforeSave: false })
+        return { refreshToken, accessToken }
 
     } catch (error) {
-        throw new APIError(500,'Somthing went Wrong while Genrating Access and Resfresh token');
+        throw new APIError(500, 'Somthing went Wrong while Genrating Access and Resfresh token');
     }
 };
 
 
-const registerUser=asyncHandler(async(req,res)=>{
+const registerUser = asyncHandler(async (req, res) => {
 
-   /*1.get user details from frontend
-    2.validate data- check empty
-    3. check if user already exist (email,username)
-    4. files exist or nat (avatar(compalsory),coverimg)
-    5. upload images to cloudinary
-    6. check avatar uploded successfully
-    7. create user object (mondodb obj) - create entry in db
-    8.check user creation
-    9. remove password & refresh token(  after user creation(storing to db) we get responce in that all data we get )
-    10. return responce
-   */
+    /*1.get user details from frontend
+     2.validate data- check empty
+     3. check if user already exist (email,username)
+     4. files exist or nat (avatar(compalsory),coverimg)
+     5. upload images to cloudinary
+     6. check avatar uploded successfully
+     7. create user object (mondodb obj) - create entry in db
+     8.check user creation
+     9. remove password & refresh token(  after user creation(storing to db) we get responce in that all data we get )
+     10. return responce
+    */
 
     // 1.get user details from frontend
-    const {fullName,email,username,password}=req.body
+    const { fullName, email, username, password } = req.body
     // console.log(req.files);
 
     // 2.validate data- check empty
@@ -53,58 +53,58 @@ const registerUser=asyncHandler(async(req,res)=>{
         throw new APIError(400,'Full Name is Required')
     }*/
 
-    if(
-        [fullName,email,username,password].some((field)=>field?.trim()==='')
-    ){
-        throw new APIError(400,'All fields are require')
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === '')
+    ) {
+        throw new APIError(400, 'All fields are require')
     }
-    
+
 
     // 3. check if user already exist (email,username)
     // below checking user is already exist or not on the basis of email& username so $or operator is used
-    const existedUser=await User.findOne({
-        $or: [{username},{email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
-    if(existedUser){
-        throw new APIError(409,'User with email or usernmae already exist!!!')
+    if (existedUser) {
+        throw new APIError(409, 'User with email or usernmae already exist!!!')
     }
 
 
     // 4. files exist or nat (avatar(compalsory),coverimg)
 
-    const coverImageFile=req.files?.coverImage[0];
+    const coverImageFile = req.files?.coverImage[0];
 
     //multer (middleware give this access to files )
     //in that we get array of that file and we are takaing llocal path from that 
-    
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
     // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
-    const coverImageLocalPath=coverImageFile?coverImageFile.path:null;
-    
-    if(!avatarLocalPath){
-        throw new APIError(400,'Avatar file Required')
+    const coverImageLocalPath = coverImageFile ? coverImageFile.path : null;
+
+    if (!avatarLocalPath) {
+        throw new APIError(400, 'Avatar file Required')
     }
 
 
     // 5. upload images to cloudinary
 
-    const avatar= await uploadOnCloudinary(avatarLocalPath)
-    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
     // console.log(coverImage);
-    if(!avatar){
-        throw new APIError(400,'Avatar file Required')
+    if (!avatar) {
+        throw new APIError(400, 'Avatar file Required')
     }
 
 
     // 7. create user object (mondodb obj) - create entry in db
-    const user= await User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
-        coverImage: coverImage?.url || '' ,     //if cover image if available then upload url or empty
+        coverImage: coverImage?.url || '',     //if cover image if available then upload url or empty
         email,
         password,
-        username:username.toLowerCase()
+        username: username.toLowerCase()
     })
 
     //8.check user creation
@@ -114,17 +114,17 @@ const registerUser=asyncHandler(async(req,res)=>{
     // select  method is used to specify which field whant to exclude or include  
     // syntax is unique like i wnat to exclude password and refreshtoken so - sign is given and fields are seprated by space
     const createdUser = await User.findById(user._id).select("-password -refreshToken")
-    if(!createdUser){
-        throw new APIError(500,'Something is went wrong while Registering the User')
+    if (!createdUser) {
+        throw new APIError(500, 'Something is went wrong while Registering the User')
     }
 
     return res.status(201).json(
-        new APIResponce(200, createdUser,"User Registered Successfully")
+        new APIResponce(200, createdUser, "User Registered Successfully")
     )
 
 })
 
-const loginUser=asyncHandler(async(req,res)=>{
+const loginUser = asyncHandler(async (req, res) => {
     /*
         1.get credientails
         2.validate credientails (email-username)
@@ -132,10 +132,10 @@ const loginUser=asyncHandler(async(req,res)=>{
         4.password check
         5.set access and refresh token
         6.send cookies 
-    */ 
+    */
 
-        // 1.get credientails
-    const {email,username,password} = req.body
+    // 1.get credientails
+    const { email, username, password } = req.body
     console.log(email);
     // 2.validate credientails (email-username)
 
@@ -144,18 +144,18 @@ const loginUser=asyncHandler(async(req,res)=>{
     //     throw new APIError(400,'Username & Email Required!!!')
     // }
 
-    if(!(email || username || password )){
-        throw new APIError(400,'Username & Password Required!!!')
+    if (!(email || username || password)) {
+        throw new APIError(400, 'Username & Password Required!!!')
     }
 
     // 3.find user
     // User.findOne() this method is mongoose buildin methods 
-    const user= await User.findOne({
-        $or: [{username},{email}]
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
     })
-    
-    if(!user){
-        throw new APIError(404,'User not Found!!!')
+
+    if (!user) {
+        throw new APIError(404, 'User not Found!!!')
     }
 
     //4.password check
@@ -163,14 +163,14 @@ const loginUser=asyncHandler(async(req,res)=>{
     //to access these method we need to use user not User
     //User is mongodb object
     //user is our object in that 
-    const isPasswordValid=await user.isPasswordCorrect(password)
-    
-    if(!isPasswordValid){
-        throw new APIError(401,'Invalid user Credintials!!!')
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new APIError(401, 'Invalid user Credintials!!!')
     }
 
     // 5.set access and refresh token
-    const {refreshToken,accessToken}= await genrateAccessAndRefreshToken(user._id);
+    const { refreshToken, accessToken } = await genrateAccessAndRefreshToken(user._id);
 
 
     // 6.send cookies 
@@ -179,43 +179,43 @@ const loginUser=asyncHandler(async(req,res)=>{
     // console.log(loggedInUser);
     //cookie options - by default cookie can modified at client side 
     //so this options are require to prevent from modification on clinet side but from server side we can modify
-    const options={
+    const options = {
         httpOnly: true,
         secure: true
     }
 
     return res
         .status(201)
-        .cookie("accessToken",accessToken,options)
-        .cookie('refreshToken',refreshToken,options)
+        .cookie("accessToken", accessToken, options)
+        .cookie('refreshToken', refreshToken, options)
         .json(
             new APIResponce(200,
-                { 
-                    user:loggedInUser, accessToken, refreshToken
+                {
+                    user: loggedInUser, accessToken, refreshToken
                 },
                 "User Logged In Successfully..."
             )
         )
 })
 
-const logoutUser=asyncHandler(async(req,res)=>{
+const logoutUser = asyncHandler(async (req, res) => {
     //req.user our object add from auth.middleware
     // console.log(req.user);
-    await User.findByIdAndUpdate(     
+    await User.findByIdAndUpdate(
         req.user._id,
-        {   
+        {
             //$_set is a mongodb opertor - give object to update values in db 
             $set: {
                 refreshToken: undefined
             }
-            
+
         },
         {
             //this new option is there this provide updated db object in return refrance
             new: true
         }
     )
-    const options={
+    const options = {
         httpOnly: true,
         secure: true,
         sameSite: 'Strict'  //Adding sameSite: 'Strict' to prevent CSRF attacks.
@@ -223,13 +223,58 @@ const logoutUser=asyncHandler(async(req,res)=>{
 
     return res
         .status(200)
-        .clearCookie('accessToken',options)
-        .clearCookie('refreshToken',options)
-        .json(new APIResponce(200,{},"User  Logged Out Successfully!!!"))
+        .clearCookie('accessToken', options)
+        .clearCookie('refreshToken', options)
+        .json(new APIResponce(200, {}, "User  Logged Out Successfully!!!"))
+})
+
+//this controller for taking new accesstoken from server 
+//accesstoken expiry is limited thats why if user is active at that time accesstoken expiried then that automatically 
+// new accesstoken get to user in background so this endpoint is required 
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!incomingRefreshToken) {
+        throw new APIError(401, 'Unauthorized Request')
+    }
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken._id);
+
+        if (!user) {
+            throw new APIError(401, 'Invalid Refresh Token')
+        }
+
+        //incoomingRT is fromuser cide
+        //user.refreshT is frm current user data from db
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new APIError(401, 'Refresh token is expired or used')
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+
+        const { newRefreshToken, newAccessToken } = await genrateAccessAndRefreshToken(user._id);
+
+        return res
+            .status(200)
+            .cookie('accessToken', newRefreshToken)
+            .cookie('refreshToken', newAccessToken)
+            .json(new APIResponce(200, { newAccessToken, newRefreshToken }, 'Access Token Refreshed...'))
+
+    } catch (error) {
+        throw new APIError(401, error?.message || 'Invalid Refresh Token')
+    }
 })
 
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken,
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js';
 import {upload} from '../middleware/multer.middleware.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
 
@@ -27,5 +27,6 @@ router.route("/login").post(loginUser);
 //secured Routes
 
 router.route("/logout").post(verifyJWT, logoutUser);      // midddleware added  
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
